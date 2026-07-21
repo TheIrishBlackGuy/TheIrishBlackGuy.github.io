a561bb6d84ed363d2a92af57aa428aa2768ed828
TheBlackGuy23 <lyndonlivingston@gmail.com>
2026-07-20 23:45:58 -0700

    adding bunch o' files
    - daria puzzle page
    - jenica puzzle page
    - mal and lyndon page but no content
    
    - stupid coords arent working for map

---
+++ random/scratchdisk-cybercritter/basic.js
@@ -0,0 +1,36 @@
+// console.log('mouseX is ' + mouseX + ' and mouseY is' + mouseY)
+
+
+
+// function setup() {
+//     createCanvas(9000, 9000);
+//     //   background(20);
+//     //   //fill(255);
+//     //   ellipseMode(CENTER);
+//     //   textAlign(CENTER, CENTER);
+//     //   textFont(f);
+// }
+
+function draw() {
+    // let mx = mouseX;
+    // let my = mouseY;
+    // if (mouseIsPressed) {
+    //     console.log(mx)
+    //     console.log(my)
+
+    //     console.log('mouseX is ' + mouseX + ' and mouseY is ' + (mouseY-.859375))
+    // }
+}
+
+function keyPressed() {
+    if ( key == 'q' || key == 'Q' ) {
+        console.log('mouseX is ' + mouseX + ' and mouseY is ' + (mouseY-.859375))
+
+  // if ( mouseIsPressed && key == 'c' || key == 'C' ) {
+  //      background(255,160,48);
+  //      textSize(150);
+  //      textFont(chaos);
+  //      text("CHAOS");
+     }
+     
+}
