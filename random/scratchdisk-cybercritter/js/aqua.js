c173bd865d834896f9f369a0c3bd1b362c2c465f
TheBlackGuy23 <lyndonlivingston@gmail.com>
2026-08-04 05:24:37 -0700

    adjusting aquarium
    - crab canvas you can draw on
    - redirected to new page to do puzzle(easier than pop up)

--- random/scratchdisk-cybercritter/js/aqua.js
+++ random/scratchdisk-cybercritter/js/aqua.js
@@ -1,42 +1,54 @@
 
 let currentkey = '1';
-let bgc ;
+let bgc;
 let gkcount;
 let ranredcount;
 
+let img;
+
+function preload() {
+
+  aqua = loadImage("assets/nonogram-crab.png")
+}
+
+
 function setup() {
-  createCanvas(400, 300);
+
+  // img =  loadImage('/assets/nonogram-crab.png');
+  let cnv = createCanvas(750, 750);
+  cnv.position((width / 2) + 50, (height / 2) - 250);
   background(224);
+  image(aqua, 0, 0, 750, 750);
   smooth();
-    bgc = color(224);
-    gkcount = 20;
-    rectMode(CENTER);
-    textAlign(CENTER)
+  bgc = color(224);
+  gkcount = 20;
+  rectMode(CENTER);
+  textAlign(CENTER)
 }
 
 
 function draw() {
-    // triggering the clear_print function
-    
-    if( keyIsPressed) {
-      clear_print();
-    }
-    // triggering the newkeychoice
-    if(mouseIsPressed) {
-     drawChoice();
-    }
+  // triggering the clear_print function
+
+  if (keyIsPressed) {
+    clear_print();
+  }
+  // triggering the newkeychoice
+  if (mouseIsPressed) {
+    drawChoice();
+  }
 }
 
-   //rect(0,0,20,20); // 4 arg
+//rect(0,0,20,20); // 4 arg
 
 
-  //  function rect(lx,ly,w,h) {
+//  function rect(lx,ly,w,h) {
 
-  //   // magial rectangle
+//   // magial rectangle
 
-  //  }
+//  }
 
-      // wrapper function ( no parameters or arguments )
+// wrapper function ( no parameters or arguments )
 function drawChoice() {
   // the key mapping if statemens that you can change to do anything you want.
   // just make sure each key option has the a stroke or fill and then what type of
@@ -44,89 +56,98 @@ function drawChoice() {
   // key global variable contains whatever key was last pressed
   let currentkey = key;
 
-switch(currentkey) {
-  case '1': // black line
-    console.log("1");  
-   // let k = color(0);
-   strokeWeight(4)
-   drawFatLine(color(0), mouseX, mouseY, pmouseX, pmouseY);
-    break;
-  case '2': // white line
-    console.log("2");  
-   // let k = color(0);
-   strokeWeight(4)
-   drawFatLine(color(255), mouseX, mouseY, pmouseX, pmouseY);
-    break;
-  case '3': // magenta line
-    console.log("3");  
-    drawLine(color(255,0,255), mouseX, mouseY, pmouseX, pmouseY);
-    break;
-  case '4': // yellow line
-    console.log("4");  
-    drawLine(color(255,255,0), mouseX, mouseY, pmouseX, pmouseY);
-    break;
-  case '5': // teal line
-    console.log("5");  
-    drawLine(color(0,255,255), mouseX, mouseY, pmouseX, pmouseY);
-    break;
-  case '6': // sparkler
-    console.log("6");  
-    drawlineSpark(color(255,128,64), mouseX, mouseY, pmouseX, pmouseY);
-    drawlineSpark(color(64,255,128), mouseX, mouseY, pmouseX, pmouseY);
-    drawlineSpark(color(128,64,255), mouseX, mouseY, pmouseX, pmouseY);
-    drawlineSpark(color(0,128,192), mouseX, mouseY, pmouseX, pmouseY);
-    drawlineSpark(color(192,0,128), mouseX, mouseY, pmouseX, pmouseY);
-    drawlineSpark(color(128,192,0), mouseX, mouseY, pmouseX, pmouseY);
-  break;
-  case '7': // cubic
-    console.log("7");  
-    fill(color(random(255),random(255),random(255)))
-    drawCubicLine(color(random(255),random(255),random(255)), mouseX, mouseY, pmouseX, pmouseY);
-    break;
-  case '8': // circ
-    console.log("8");  
-    fill(random(255),random(255),random(255));
-    drawCircleLine(mouseX, mouseY, pmouseX, pmouseY);
-    break;
-  case '9': // 180 circ
-    console.log("9");  // 180 circle
-    fill(random(255),random(255),random(255));
-    drawSpiralLine(mouseX, mouseY, pmouseX, pmouseY);
-    break;
+  switch (currentkey) {
+    case '3': // black line
+      console.log("3");
+      // let k = color(0);
+      strokeWeight(4)
+      drawFatLine(color(0), mouseX, mouseY, pmouseX, pmouseY);
+      break;
+    case '4': // white line
+      console.log("4");
+      // let k = color(0);
+      strokeWeight(4)
+      drawFatLine(color(255), mouseX, mouseY, pmouseX, pmouseY);
+      break;
+    // case '3': // magenta line
+    //   console.log("3");
+    //   drawLine(color(255, 0, 255), mouseX, mouseY, pmouseX, pmouseY);
+    //   break;
+    // case '4': // yellow line
+    //   console.log("4");
+    //   drawLine(color(255, 255, 0), mouseX, mouseY, pmouseX, pmouseY);
+    //   break;
+    // case '5': // teal line
+    //   console.log("5");
+    //   drawLine(color(0, 255, 255), mouseX, mouseY, pmouseX, pmouseY);
+    //   break;
+    // case '6': // sparkler
+    //   console.log("6");
+    //   drawlineSpark(color(255, 128, 64), mouseX, mouseY, pmouseX, pmouseY);
+    //   drawlineSpark(color(64, 255, 128), mouseX, mouseY, pmouseX, pmouseY);
+    //   drawlineSpark(color(128, 64, 255), mouseX, mouseY, pmouseX, pmouseY);
+    //   drawlineSpark(color(0, 128, 192), mouseX, mouseY, pmouseX, pmouseY);
+    //   drawlineSpark(color(192, 0, 128), mouseX, mouseY, pmouseX, pmouseY);
+    //   drawlineSpark(color(128, 192, 0), mouseX, mouseY, pmouseX, pmouseY);
+    //   break;
+    case '1': // cubic
+      console.log("1");
+      // fill(color(random(255), random(255), random(255)))
+      fill(color(0, 0, 0))
+      // drawCubicLine(color(random(255), random(255), random(255)), mouseX, mouseY, pmouseX, pmouseY);
+      drawCubicLine(color(0, 0, 0), mouseX, mouseY, pmouseX, pmouseY);
+      break;
+    case '2': // cubic
+      console.log("2");
+      // fill(color(random(255), random(255), random(255)))
+      fill(color(0, 0, 192))
+      // drawCubicLine(color(random(255), random(255), random(255)), mouseX, mouseY, pmouseX, pmouseY);
+      drawCubicLine(color(0, 0, 192), mouseX, mouseY, pmouseX, pmouseY);
+      break;
+    // case '8': // circ
+    //   console.log("8");
+    //   fill(random(255), random(255), random(255));
+    //   drawCircleLine(mouseX, mouseY, pmouseX, pmouseY);
+    //   break;
+    // case '9': // 180 circ
+    //   console.log("9");  // 180 circle
+    //   fill(random(255), random(255), random(255));
+    //   drawSpiralLine(mouseX, mouseY, pmouseX, pmouseY);
+    //   break;
 
-  case 'r': // spotty red
-    console.log("r");  // spotty red
-    fill(random(255),0,0);
-    drawRandomCircle((random(255),0,0), mouseX, mouseY, pmouseX, pmouseY);
-    break;
-  case 'g': // spotty green
-    console.log("g");  // spotty red
-    fill(0,random(255),0);
-    drawRandomCircle((0,random(255),0), mouseX, mouseY, pmouseX, pmouseY);
-    break;
-  case 'b': // spotty blue
-    console.log("b");  // spotty red
-    fill(0,0,random(255));
-    drawRandomCircle((0,0,random(255)), mouseX, mouseY, pmouseX, pmouseY);
-    break;
-  case 'c': // spotty blue
-    console.log("c");  // spotty red
-    fill(random(255),random(255),random(255));
-    drawRandomCircle((random(255),random(255),random(255)), mouseX, mouseY, pmouseX, pmouseY);
-    break;
-    
-case 'e': // eraser
-    console.log("e");  
-    eraser(bgc,mouseX, mouseY,100);
-    break;
-  
-case 'f': // eraser
-    console.log("f");  
-    background(32);
-    break;
+    // case 'r': // spotty red
+    //   console.log("r");  // spotty red
+    //   fill(random(255), 0, 0);
+    //   drawRandomCircle((random(255), 0, 0), mouseX, mouseY, pmouseX, pmouseY);
+    //   break;
+    // case 'g': // spotty green
+    //   console.log("g");  // spotty red
+    //   fill(0, random(255), 0);
+    //   drawRandomCircle((0, random(255), 0), mouseX, mouseY, pmouseX, pmouseY);
+    //   break;
+    // case 'b': // spotty blue
+    //   console.log("b");  // spotty red
+    //   fill(0, 0, random(255));
+    //   drawRandomCircle((0, 0, random(255)), mouseX, mouseY, pmouseX, pmouseY);
+    //   break;
+    // case 'c': // spotty blue
+    //   console.log("c");  // spotty red
+    //   fill(random(255), random(255), random(255));
+    //   drawRandomCircle((random(255), random(255), random(255)), mouseX, mouseY, pmouseX, pmouseY);
+    //   break;
+
+    case 'e': // eraser
+      console.log("e");
+      eraser(bgc, mouseX, mouseY, 100);
+      break;
+
+    case 'f': // eraser
+      console.log("f");
+      background(32);
+      break;
   }
 
-//default:             // Default executes if the case labels
+  //default:             // Default executes if the case labels
   console.log("None");   // don't match the switch parameter
 
 }
@@ -134,8 +155,8 @@ case 'f': // eraser
 //lx,ly:current location
 //px,py:previous location
 //connects the points
-function drawLine( k,  lx, ly,  px, py) {
-  
+function drawLine(k, lx, ly, px, py) {
+
   strokeWeight(5);
   stroke(k);
   line(lx, ly, px, py);
@@ -144,8 +165,8 @@ function drawLine( k,  lx, ly,  px, py) {
 
 }
 
-function drawCircleLine ( k,  lx, ly, px, py) {
-  
+function drawCircleLine(k, lx, ly, px, py) {
+
   strokeWeight(1);
   stroke(1);
   ellipse(mouseX, mouseY, 25, 25);
@@ -154,12 +175,12 @@ function drawCircleLine ( k,  lx, ly, px, py) {
 
 }
 
-function drawSpiralLine ( k,  lx, ly, px, py) {
-  
+function drawSpiralLine(k, lx, ly, px, py) {
+
   strokeWeight(1);
   stroke(1);
   ellipse(mouseX, mouseY, 25, 25);
-  ellipse(1000-mouseX, 750-mouseY, 25, 25);
+  ellipse(1000 - mouseX, 750 - mouseY, 25, 25);
   // ellipse(mouseX/2, mouseY/2, 25, 25);
   // ellipse((1000-mouseX), 1000-mouseY, 25, 25);
   console.log(mouseX);
@@ -167,44 +188,45 @@ function drawSpiralLine ( k,  lx, ly, px, py) {
 
 }
 
-function drawCubicLine ( k,  lx, ly, px, py) {
-  
+function drawCubicLine(k, lx, ly, px, py) {
+
   strokeWeight(1);
-  stroke(random(255),random(255),random(255));
-  rect(lx, ly, random(100), random(100));
+  // stroke(random(255), random(255), random(255));
+  stroke(0, 0, 0);
+  rect(lx, ly, 30, 30);
   console.log(mouseX);
   console.log(pmouseX);
 
 }
 
-function drawlineSpark( k,  lx, ly,  px, py) {
-  
+function drawlineSpark(k, lx, ly, px, py) {
+
   strokeWeight(1);
   stroke(k);
-  line(lx+random(-100,100), ly+random(-100,100), px, py);
-  line(lx+random(-100,100), ly+random(-100,100), px, py);
-  line(lx+random(-100,100), ly+random(-100,100), px, py);
+  line(lx + random(-100, 100), ly + random(-100, 100), px, py);
+  line(lx + random(-100, 100), ly + random(-100, 100), px, py);
+  line(lx + random(-100, 100), ly + random(-100, 100), px, py);
   console.log(mouseX);
   console.log(pmouseX);
 }
 
-function drawFatLine( k,  lx, ly,  px, py) {
-  strokeWeight(10);
+function drawFatLine(k, lx, ly, px, py) {
+  strokeWeight(25);
   stroke(k);
   line(lx, ly, px, py);
 }
 
-function eraser( k, lx, ly, sz) {
+function eraser(k, lx, ly, sz) {
   fill(k);
   stroke(k);
-  ellipse(lx, ly, sz,sz);
+  ellipse(lx, ly, sz, sz);
 }
 
-function drawRandomCircle( k,  lx, ly,  px, py) {
-  
+function drawRandomCircle(k, lx, ly, px, py) {
+
   strokeWeight(1);
   stroke(k);
-  ellipse(lx+random(-20,20), ly+random(-20,20), 20, 20);
+  ellipse(lx + random(-20, 20), ly + random(-20, 20), 20, 20);
   console.log(mouseX);
   console.log(pmouseX);
 }
