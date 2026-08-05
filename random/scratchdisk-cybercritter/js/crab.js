50527514542e19f80546e4ab4abdd403235f922a
TheBlackGuy23 <lyndonlivingston@gmail.com>
2026-08-05 03:29:49 -0700

    adjusting crab
    - tweaked marker opacity
    - added red marker
    - cleaned js code
    - shifted <div>

---
+++ random/scratchdisk-cybercritter/js/crab.js
@@ -0,0 +1,77 @@
+let currentkey = '1';
+
+function preload() {
+  aqua = loadImage("assets/nonogram-crab.png")
+}
+
+function setup() {
+  let cnv = createCanvas(750, 750);
+  cnv.position((width / 2) + 50, (height / 2) - 250);
+  image(aqua, 0, 0, 750, 750);
+  smooth();
+  rectMode(CENTER);
+  textAlign(CENTER);
+}
+
+function draw() {
+  if (keyIsPressed) {
+    clear_print();
+  }
+  if (mouseIsPressed) {
+    drawChoice();
+  }
+
+  {
+    for (let i = 0; i < 500; i += 32.5) {
+      strokeWeight(2);
+      line(227 + i, 227, 227 + i, 715);
+      line(227, 227 + i, 715, 227 + i);
+    }
+  }
+}
+
+function drawChoice() {
+  let currentkey = key;
+  switch (currentkey) {
+    case '1': // cubic
+      console.log("1");
+      fill(color(0, 0, 128, 64))
+      drawCubicLine(color(0, 0, 0), mouseX, mouseY, pmouseX, pmouseY);
+      break;
+    case '2': // cubic
+      console.log("2");
+      fill(color(32, 96, 192, 96))
+      drawCubicLine(color(0, 0, 192), mouseX, mouseY, pmouseX, pmouseY);
+      break;
+    case '3': // cubic
+      console.log("3");
+      fill(color(192, 0, 0, 16))
+      drawCubicLine(color(0, 0, 192), mouseX, mouseY, pmouseX, pmouseY);
+      break;
+  }
+}
+
+function drawLine(k, lx, ly, px, py) {
+  strokeWeight(5);
+  stroke(k);
+  line(lx, ly, px, py);
+  console.log(mouseX);
+  console.log(pmouseX);
+}
+
+function drawCubicLine(k, lx, ly, px, py) {
+  strokeWeight(0);
+  rect(lx, ly, 30, 30);
+  console.log('X ' + mouseX);
+  console.log('Y ' + mouseY);
+}
+
+function clear_print() {
+  if (key == 'p' || key == 'P') {
+    saveFrames('image-0', 'png', 1, 1);
+    key = '';  // resets the key so it does not make more than one image.
+  }
+}
+
+// 227 260 292 325 357 389 422 454 486 519 552 584 616 650 682 715
+// __ 33  32  33  32  32  33  32  32  33  33  32  32  34  32  33  = 32.5
