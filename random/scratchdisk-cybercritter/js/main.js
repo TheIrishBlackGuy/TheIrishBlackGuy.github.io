34860b0de21389ba14074b45bbb69b8ef632b151
TheBlackGuy23 <lyndonlivingston@gmail.com>
2026-08-17 15:16:38 -0700

    safari animals
    - added Cat's animals
    - added Daria's text to main
    - reordered main <div>s

--- random/scratchdisk-cybercritter/js/main.js
+++ random/scratchdisk-cybercritter/js/main.js
@@ -1,12 +1,12 @@
 function checkInput() {
     var userN = document.getElementById('username').value;
-    if (userN === "tropic" || userN === "Tropic" || userN === "TROPIC" || userN === "tropic " || userN === "Tropic " || userN === "TROPIC ") {
+    if (userN === "acacia" || userN === "Acacia" || userN === "ACACIA" || userN === "acacia " || userN === "Acacia " || userN === "ACACIA ") {
         var userN1 = document.getElementById('username1').value;
-        if (userN1 === "aqua" || userN1 === "Aqua" || userN1 === "AQUA" || userN1 === "aqua " || userN1 === "Aqua " || userN1 === "AQUA ") {
+        if (userN1 === "kelp" || userN1 === "Kelp" || userN1 === "KELP" || userN1 === "kelp " || userN1 === "Kelp " || userN1 === "KELP ") {
             var userN2 = document.getElementById('username2').value;
-            if (userN2 === "farm" || userN2 === "Farm" || userN2 === "FARM" || userN2 === "farm " || userN2 === "Farm " || userN2 === "FARM ") {
+            if (userN2 === "wheat" || userN2 === "Wheat" || userN2 === "WHEAT" || userN2 === "wheat " || userN2 === "Wheat " || userN2 === "WHEAT ") {
                 var userN3 = document.getElementById('username3').value;
-                if (userN3 === "green" || userN3 === "Green" || userN3 === "GREEN" || userN3 === "green " || userN3 === "Green " || userN3 === "GREEN ") {
+                if (userN3 === "orchid" || userN3 === "Orchid" || userN3 === "ORCHID" || userN3 === "orchid " || userN3 === "Orchid " || userN3 === "ORCHID ") {
 
                     document.location = 'terminal.html';
 
